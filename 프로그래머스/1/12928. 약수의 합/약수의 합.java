e3841ddcfcae5618ac755dfe26eca9e431696a96
고대일 <129933421+found-rorre@users.noreply.github.com>
2024-02-15 22:09:29 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;
+        ArrayList<Integer> divisor = new ArrayList<Integer>();
+        
+	    switch (n%2) {
+		
+            case 0:
+                for(int i = 1; i <= n/2; i++) {
+                    if( n % i == 0 ) {
+                        divisor.add(i);
+                    }
+                }
+                divisor.add(n);
+                break;
+
+            default:
+                for(int i = 1; i <= n; i++) {
+                    if( n % i == 0 ) {
+                        divisor.add(i);
+                    }
+                }
+                break;
+		}
+		
+		for (Integer div : divisor) {
+			answer += div;
+		}
+		
+
+        
+        
+        
+        
+        
+
+        return answer;
+    }
+}
