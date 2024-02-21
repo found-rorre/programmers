3cac2d69e596dc416b3e9d6b2aed6ce9e2477065
고대일 <129933421+found-rorre@users.noreply.github.com>
2024-02-21 15:05:47 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,3 +1,13 @@
+/*
+
+올바르지 않을 경우를 먼저 생각해보았다.
+1. 첫 괄호부터 닫혀있는 경우
+2. 중간이라도 닫힌 괄호가 열린 괄호보다 많은 경우
+3. 짝이 맞지 않는경우
+
+answer의 초기값을 true로 주어지고 위에 조건과 일치할 경우 false로 변환 후 return 시켰 종료했다.
+
+*/ 
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,7 +18,8 @@ class Solution {
         int flag = 0;
 		boolean answer = true;
 		List<String>list =  Arrays.asList(s.split("")); 
-		
+
+	    	// 1번
 		if(list.get(0) == ")") {
 			answer = false;
 			return answer;
@@ -20,13 +31,15 @@ class Solution {
 				flag += 1;
 			}else {
 				flag -= 1;
-				
+
+				// 2번
 				if(flag < 0) {
 					answer = false;
 					break;
 				}
 			}
 		}
+	// 3번
         answer = flag == 0 ? true : false;
         
         return answer;
