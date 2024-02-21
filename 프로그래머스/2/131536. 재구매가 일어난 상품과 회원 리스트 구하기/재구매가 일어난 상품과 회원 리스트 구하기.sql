7f398f5f29faac6c0b0f15decf5bd941df736016
고대일 <129933421+found-rorre@users.noreply.github.com>
2024-02-21 15:09:04 +0900

    Update 재구매가 일어난 상품과 회원 리스트 구하기.sql

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,3 +1,7 @@
+-- having절을 적절히 쓸줄 아는가에 대한 문제인줄 알았지만
+-- group by절에 두 인수를 넣는것이 생각보다 어려웠다.
+-- 정리가 완전히 되고 맞춘 문제는 아니라 이와 같은 예시 문제를 더 풀어봐야겠다.
+
 select user_id, product_id
 from ONLINE_SALE
 group by user_id, product_id
