98de11630fe2a46fcb45e687f8c57f7941d82793
GO Daeil <129933421+found-rorre@users.noreply.github.com>
2024-07-31 04:35:30 +0900

    [level 0] Title: 짝수는 싫어요, Time: 3.12 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -2,8 +2,6 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer = IntStream.range(1, n +1).filter(num -> (num + 1) % 2 == 0).toArray();
-
-        return answer;
+        return IntStream.rangeClosed(1, n).filter(i -> i % 2 != 0).toArray();
     }
 }
