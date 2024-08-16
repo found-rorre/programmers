69d8f7ad09f030769a7db961b8525dee9b519859
GO Daeil <129933421+found-rorre@users.noreply.github.com>
2024-08-16 13:32:35 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.82 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(String numbers) {
+        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < nums.length; i++) {
+            numbers = numbers.replaceAll(nums[i], String.valueOf(i));
+        }
+        
+        return Long.parseLong(numbers);
+    }
+}
