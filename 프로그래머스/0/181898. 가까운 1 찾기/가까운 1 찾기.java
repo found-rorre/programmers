73fb3640da2d4a20c5f29da118bf38e9e84c0479
GO Daeil <129933421+found-rorre@users.noreply.github.com>
2024-08-18 18:30:51 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.20 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,15 +1,16 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] arr, int idx) {
         
-        int answer = -1;
+        int[] copy = Arrays.copyOfRange(arr, idx, arr.length);
         
-        for(int i = idx; i < arr.length; i++) {
-            if(arr[i] == 1) {
-                answer = i;
-                break;
+        for (int i = 0; i < copy.length; i++) {
+            if (copy[i] == 1) {
+                return i + idx;
             }
         }
         
-        return answer;
+        return -1;
     }
 }
