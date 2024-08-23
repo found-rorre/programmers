e33010ed4ed3eee72ed1b1dfcc3fe8e61cbd9685
GO Daeil <129933421+found-rorre@users.noreply.github.com>
2024-08-23 12:43:02 +0900

    [level 0] Title: 5명씩, Time: 0.31 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -3,14 +3,25 @@ import java.util.List;
 
 class Solution {
     public String[] solution(String[] names) {
-        List<String> list = new ArrayList();
+//         List<String> list = new ArrayList();
+        
+//         int group = 5;
+
+//         for (int i = 0; i < names.length; i += group) {
+//             list.add(names[i]);
+//         }
+
+//         return list.toArray(String[]::new);
         int group = 5;
+        int len = (int) Math.ceil( (double) names.length / group);
+        int idx = 0;
+        String[] result = new String[len];
 
         for (int i = 0; i < names.length; i += group) {
-            list.add(names[i]);
+            result[idx++] = names[i]; 
         }
 
-        return list.toArray(String[]::new);
+       return result;
 
     }
 }
