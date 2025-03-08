de4e098a7d9a5b588b553839be75dc409a007964
GO Daeil <129933421+found-rorre@users.noreply.github.com>
2025-03-08 17:03:50 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,9 +1,5 @@
--- having절을 적절히 쓸줄 아는가에 대한 문제인줄 알았지만
--- group by절에 두 인수를 넣는것이 생각보다 어려웠다.
--- 정리가 완전히 되고 맞춘 문제는 아니라 이와 같은 예시 문제를 더 풀어봐야겠다.
-
-select user_id, product_id
-from ONLINE_SALE
-group by user_id, product_id
-having count(product_id) > 1
-order by user_id, product_id desc
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE 
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) > 1
+ORDER BY USER_ID ASC, PRODUCT_ID DESC
